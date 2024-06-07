c80f9f4607bb73a57cf8751e7a688385d6d9e351
Martin Hiner <martin.hiner@tietoevry.com>
2024-06-07 13:35:56 +0200

    Add custom error struct
    Adds a file with custom error struct used for signaling if
    query should be refused.

---
+++ dns_filter_server/dns_errors.go
@@ -0,0 +1,24 @@
+package dns_filter_server
+
+type dns_error struct {
+	msg string
+	filtered bool
+}
+
+
+func NewDNSError(msg string, filtered bool) (*dns_error) {
+	return &dns_error{msg, filtered}
+}
+
+func RepackDNSError(e error, filtered bool) (*dns_error) {
+	return &dns_error{e.Error(), filtered}
+}
+
+
+func (e *dns_error) Error() string {
+	return e.msg
+}
+
+func (e *dns_error) Filtered() bool {
+	return e.filtered
+}
